ea8816ae880d0720622cb7c5e96b21d48c4532a2
KaleemSipra <145860468+Mkzsipra@users.noreply.github.com>
2023-12-23 13:50:24 +0500

    Update in Script.js file for Some navbar issue

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ let navLinks = document.getElementById("navlinks");
 menuBtn.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+
+let li = document.querySelectorAll('li');
+li.forEach((li)=>{
+  li.addEventListener('click', ()=>{
+    // console.log("yes");
+    navLinks.classList.toggle("active");
+  })
+})
